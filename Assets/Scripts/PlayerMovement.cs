3db806729bcb5d37df7f7269e017aaf291e41630
WhisperCheng <sr.cheese44@gmail.com>
2024-02-22 12:36:08 +0000

    prototipado de movimiento
    velocidad y rotacion de la nave

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    [SerializeField]
+    public float playerSpeed;
+    public Vector2 turn;
+    public float turnSpeed = .5f;
+    public float minSpeed;
+    public float maxSpeed;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * playerSpeed * Time.deltaTime;
+
+        turn.x += Input.GetAxis("Mouse X") * turnSpeed;
+        turn.y += Input.GetAxis("Mouse Y") * turnSpeed;
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerSpeed +=  5 * Time.deltaTime;
+
+            if(playerSpeed > maxSpeed) 
+            {
+                playerSpeed = maxSpeed;
+            }
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            playerSpeed -= 5 * Time.deltaTime;
+
+            if(playerSpeed < minSpeed)
+            {
+                playerSpeed = minSpeed;
+            }
+        }
+    }
+
+
+
+}
